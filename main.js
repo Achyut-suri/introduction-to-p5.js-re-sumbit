6626ea1000c370305e9292225037a6908edb04ef
Achyut-suri <88927531+Achyut-suri@users.noreply.github.com>
2021-09-12 14:57:48 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,30 +1,30 @@
-function preload()
+function Preload()
 {
 
 }
 
 function setup()
 {
-canvas=createCanvas(640, 480);
-canvas.position(110, 250);
+canvas=createCanvas(640 , 480);
+canvas.position(150 , 150);
 video=createCapture(VIDEO);
 video.hide();
-
-tint_colour = " ";
 }
 
 function draw()
 {
-image(video, 0 , 0 , 640 , 480);
-tint(tint_colour);
-}
-
-function filter_snapshot()
-{
-tint_colour=document.getElementById("colour_name").value;
+image(video, 15 , 10 , 600 , 480);
+fill(255,0,0);
+circle(100 , 50,40);
+circle(500 , 50 , 40);
+circle(100 , 450 , 40);
+circle(500 , 450 , 40);
+fill(0 , 0 , 0);
+rect(100 , 50 , 400 , 20 );
+rect(100 , 450 , 400 , 20);
 }
 
 function take_snapshot()
 {
-save('Snapshot.png');
+save("Picture.png");
 }
